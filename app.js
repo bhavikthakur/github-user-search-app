cb26854468c7ff21ac220f1a349bb809f8c9347c
Bhavik Thakur <thakursbhavik@gmail.com>
2025-05-17 04:20:11 +0530

    Update theme logic

--- app.js
+++ app.js
@@ -42,114 +42,114 @@ const moonIcon = document.querySelector(".card__theme-img--dark");
 const lightBtn = document.querySelector(".card__btn-theme--light");
 const sunIcon = document.querySelector(".card__theme-img--light");
 
+// preventing form submission
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 });
 
-themeBtn.addEventListener("click", () => {
-  darkBtn.classList.toggle("inactive--theme");
-  moonIcon.classList.toggle("inactive--theme");
-  lightBtn.classList.toggle("inactive--theme");
-  sunIcon.classList.toggle("inactive--theme");
-  document.body.classList.toggle("dark--mode");
-  form.classList.toggle("dark--mode");
-  hero.classList.toggle("dark--mode");
-  hiddenModal.classList.toggle("dark--mode");
-  input.classList.toggle("dark--mode");
-  logo.classList.toggle("dark--mode");
-  socialContainer.classList.toggle("dark--mode");
-  name.classList.toggle("dark--mode");
-  socialDescriptions.forEach((socialDescription) => {
-    socialDescription.classList.toggle("dark--mode");
-  });
-  footerLinks.forEach((footerLink) => {
-    footerLink.classList.toggle("dark--mode");
-  });
-  footerIcons.forEach((footerIcon) => {
-    footerIcon.classList.toggle("dark--mode");
-  });
+// Dark & light mode handler
 
-  themeBtns.forEach((themeBtn) => {
-    themeBtn.classList.toggle("dark--mode");
-  });
-  themeIcons.forEach((themeIcon) => {
-    themeIcon.classList.toggle("dark--mode");
-  });
+themeBtn.addEventListener("click", () => {
+  const html = document.documentElement;
+  const isDarkTheme = html.getAttribute("data-theme") === "dark";
+  if (isDarkTheme) {
+    // Light Theme set
+    html.removeAttribute("data-theme");
+    sunIcon.classList.add("inactive--theme");
+    lightBtn.classList.add("inactive--theme");
+    moonIcon.classList.remove("inactive--theme");
+    darkBtn.classList.remove("inactive--theme");
+  } else {
+    // Dark theme set
+    html.setAttribute("data-theme", "dark");
+    sunIcon.classList.remove("inactive--theme");
+    lightBtn.classList.remove("inactive--theme");
+    moonIcon.classList.add("inactive--theme");
+    darkBtn.classList.add("inactive--theme");
+  }
 });
+
+// user search function
 
 searchBtn.addEventListener("click", () => {
   let userInput = input.value;
   input.value = "";
   async function getUserInfo() {
-    const GITHUB_API = "https://api.github.com/users/";
-    const data = await fetch(`${GITHUB_API}${userInput}`);
-    const result = await data.json();
-    if (result.status === "404" && result.message === "Not Found") {
-      card.classList.add("inactive--theme");
-      hiddenModal.classList.add("active");
-    } else {
-      const userName = result.name;
-      const userID = result.login;
-      const userBio = result.bio;
-      const userDOJ = result.created_at;
-      const userRepos = result.public_repos;
-      const userFollowers = result.followers;
-      const userFollowing = result.following;
-      const userLocation = result.location;
-      const userTwitter = result.twitter_username;
-      const userBlog = result.blog;
-      const userCompany = result.company;
-      const userAvatar = result.avatar_url;
-      name.innerText = userName;
-      id.innerText = `@${userID}`;
-      // userDOJ.innerText = `Joined `
-      bio.innerText = userBio ? userBio : "This profile has no bio.";
-      repos.innerText = userRepos;
-      followers.innerText = userFollowers;
-      following.innerText = userFollowing;
-      avatar.setAttribute("src", userAvatar);
-      if (!userLocation) {
-        locationEl.innerText = "Not available";
-        locationEl.classList.add("unavailable");
-        locationIcon.classList.add("unavailable");
+    try {
+      const GITHUB_API = "https://api.github.com/users/";
+      const data = await fetch(`${GITHUB_API}${userInput}`);
+      const result = await data.json();
+      if (result.status === "404" && result.message === "Not Found") {
+        card.classList.add("inactive--theme");
+        hiddenModal.classList.add("active");
+        return;
       } else {
-        locationEl.innerText = userLocation;
-        locationEl.classList.remove("unavailable");
-        locationIcon.classList.remove("unavailable");
+        const userName = result.name;
+        const userID = result.login;
+        const userBio = result.bio;
+        const userDOJ = result.created_at;
+        const userRepos = result.public_repos;
+        const userFollowers = result.followers;
+        const userFollowing = result.following;
+        const userLocation = result.location;
+        const userTwitter = result.twitter_username;
+        const userBlog = result.blog;
+        const userCompany = result.company;
+        const userAvatar = result.avatar_url;
+        name.innerText = userName;
+        id.innerText = `@${userID}`;
+        // userDOJ.innerText = `Joined `
+        bio.innerText = userBio ? userBio : "This profile has no bio.";
+        repos.innerText = userRepos;
+        followers.innerText = userFollowers;
+        following.innerText = userFollowing;
+        avatar.setAttribute("src", userAvatar);
+        if (!userLocation) {
+          locationEl.innerText = "Not available";
+          locationEl.classList.add("unavailable");
+          locationIcon.classList.add("unavailable");
+        } else {
+          locationEl.innerText = userLocation;
+          locationEl.classList.remove("unavailable");
+          locationIcon.classList.remove("unavailable");
+        }
+        if (!userTwitter) {
+          twitterEl.innerText = "Not available";
+          twitterEl.classList.add("unavailable");
+          twitterIcon.classList.add("unavailable");
+          twitterEl.setAttribute("href", "#");
+          twitterEl.setAttribute("target", "_self");
+        } else {
+          twitterEl.innerText = userTwitter;
+          twitterEl.setAttribute("href", `https://x.com/${userTwitter}`);
+          twitterEl.classList.remove("unavailable");
+          twitterIcon.classList.remove("unavailable");
+        }
+        if (!userBlog) {
+          blogEl.innerText = "Not available";
+          blogEl.classList.add("unavailable");
+          blogIcon.classList.add("unavailable");
+          blogEl.setAttribute("href", "#");
+          blogEl.setAttribute("target", "_self");
+        } else {
+          blogEl.innerText = userBlog;
+          blogEl.setAttribute("href", `https://${userBlog}`);
+          blogEl.classList.remove("unavailable");
+          blogIcon.classList.remove("unavailable");
+        }
+        if (!userCompany) {
+          companyEl.innerText = "Not available";
+          companyEl.classList.add("unavailable");
+          companyIcon.classList.add("unavailable");
+        } else {
+          companyEl.innerText = userCompany;
+          companyEl.classList.remove("unavailable");
+          companyIcon.classList.remove("unavailable");
+        }
       }
-      if (!userTwitter) {
-        twitterEl.innerText = "Not available";
-        twitterEl.classList.add("unavailable");
-        twitterIcon.classList.add("unavailable");
-        twitterEl.setAttribute("href", "#");
-        twitterEl.setAttribute("target", "_self");
-      } else {
-        twitterEl.innerText = userTwitter;
-        twitterEl.setAttribute("href", `https://x.com/${userTwitter}`);
-        twitterEl.classList.remove("unavailable");
-        twitterIcon.classList.remove("unavailable");
-      }
-      if (!userBlog) {
-        blogEl.innerText = "Not available";
-        blogEl.classList.add("unavailable");
-        blogIcon.classList.add("unavailable");
-        blogEl.setAttribute("href", "#");
-        blogEl.setAttribute("target", "_self");
-      } else {
-        blogEl.innerText = userBlog;
-        blogEl.setAttribute("href", `https://${userBlog}`);
-        blogEl.classList.remove("unavailable");
-        blogIcon.classList.remove("unavailable");
-      }
-      if (!userCompany) {
-        companyEl.innerText = "Not available";
-        companyEl.classList.add("unavailable");
-        companyIcon.classList.add("unavailable");
-      } else {
-        companyEl.innerText = userCompany;
-        companyEl.classList.remove("unavailable");
-        companyIcon.classList.remove("unavailable");
-      }
+    } catch (error) {
+      alert("bad request, try again later");
     }
   }
 
@@ -158,7 +158,19 @@ searchBtn.addEventListener("click", () => {
 // Joined 25 Jan 2011
 // 2022-11-23T18:53:57Z
 //telishreyas10 searched, blog icon disappears
+
+// show modal when user not found
 modalBtn.addEventListener("click", () => {
   hiddenModal.classList.remove("active");
   card.classList.remove("inactive--theme");
+});
+
+// My avatar by default
+window.addEventListener("load", () => {
+  async function showDevImg() {
+    const data = await fetch("https://api.github.com/users/bhavikthakur");
+    const result = await data.json();
+    avatar.setAttribute("src", result.avatar_url);
+  }
+  showDevImg();
 });
